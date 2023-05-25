5103328cabcabf838f154f30ec999c83374f62bd
akayst <akayst@naver.com>
2023-05-25 14:57:20 +0900

    [Refactoring] 회원가입, 로그인 UI 수정, 함수분리

--- PicMap/Module/Login/loginViewController.swift
+++ PicMap/Module/Login/loginViewController.swift
@@ -10,58 +10,58 @@ import Firebase
 import FirebaseAuth
 import GoogleSignIn
 
-class loginViewController: UIViewController{
-    
+final class loginViewController: UIViewController {
  
-    @IBOutlet weak var emailTextfield: UITextField!
-    
-    @IBOutlet weak var passwordTextfield: UITextField!
-    
-    @IBOutlet var loginBtn: UIButton!
-    
-    @IBOutlet var registerBtn: UIButton!
+    @IBOutlet private weak var emailTextfield: UITextField!
+    @IBOutlet private weak var passwordTextfield: UITextField!
+    @IBOutlet private weak var loginBtn: UIButton!
+    @IBOutlet private weak var registerBtn: UIButton!
 
     @IBAction func googlebb(_ sender: Any) {
         let user = AppDelegate.user
         emailTextfield.text = user?.profile.email
     }
     
-    @IBOutlet weak var GsiBtn: GIDSignInButton!
+    @IBOutlet private weak var GsiBtn: GIDSignInButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        GIDSignIn.sharedInstance()?.presentingViewController = self
-        GIDSignIn.sharedInstance().signIn()
-        GsiBtn.style = .standard
-        navigationController?.setNavigationBarHidden(true, animated: false)
-        //if let _ = UserDefaults.standard.string(forKey: "userEmail") {
-       //     performSegue(withIdentifier: "preLoginToMap", sender: self)
-       // }
-        loginBtn.layer.cornerRadius = 10
-        loginBtn.clipsToBounds = true
-        registerBtn.layer.cornerRadius = 10
-        registerBtn.clipsToBounds = true
-        emailTextfield.backgroundColor = .white
-        passwordTextfield.backgroundColor = .white
-        emailTextfield.textAlignment = .center
-        passwordTextfield.textAlignment = .center
-        self.navigationItem.title = ""
+        setupGoogleSign()
+        setupView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         let user = AppDelegate.user
         emailTextfield.text = user?.profile.email
     }
+    
+    private func setupView() {
+        loginBtn.layer.cornerRadius = 10
+        loginBtn.clipsToBounds = true
+        registerBtn.layer.cornerRadius = 10
+        registerBtn.clipsToBounds = true
+        emailTextfield.backgroundColor = .lightGray
+        passwordTextfield.backgroundColor = .lightGray
+        emailTextfield.textAlignment = .center
+        passwordTextfield.textAlignment = .center
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        self.navigationItem.title = ""
+    }
+    
+    private func setupGoogleSign() {
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance().signIn()
+        GsiBtn.style = .standard
+    }
 
     @IBAction func loginPressed(_ sender: UIButton) {
-        
-        if let email = emailTextfield.text, let password = passwordTextfield.text{
-            Auth.auth().signIn(withEmail: email, password: password) {[weak self] authResult, error in
-                if let e = error{
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                if let e = error {
                     print(e)
-                }else{
+                } else {
                     UserDefaults.standard.set(email, forKey: "userEmail")
-                    self!.performSegue(withIdentifier: "loginToMap", sender: self)
+                    self?.performSegue(withIdentifier: "loginToMap", sender: self)
                 }
             }
         }
--- PicMap/Module/Login/registerViewController.swift
+++ PicMap/Module/Login/registerViewController.swift
@@ -8,36 +8,34 @@
 import UIKit
 import Firebase
 import FirebaseAuth
-class registerViewController: UIViewController{
-    
 
-    @IBOutlet weak var emailTextfield: UITextField!
+final class registerViewController: UIViewController {
     
+    @IBOutlet private weak var emailTextfield: UITextField!
+    @IBOutlet private weak var passwordTextfield: UITextField!
+    @IBOutlet private weak var registerBtn: UIButton!
     
-    @IBOutlet weak var passwordTextfield: UITextField!
-    
-    
-    
-    @IBOutlet var registerBtn: UIButton!
-    
-
     override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
         registerBtn.layer.cornerRadius = 10
         registerBtn.clipsToBounds = true
-        emailTextfield.backgroundColor = .white
-        passwordTextfield.backgroundColor = .white
+        emailTextfield.backgroundColor = .lightGray
+        passwordTextfield.backgroundColor = .lightGray
         emailTextfield.textAlignment = .center
         passwordTextfield.textAlignment = .center
     }
 
     @IBAction func registerPressed(_ sender: Any) {
-        if let email = emailTextfield.text , let password = passwordTextfield.text{
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        if let email = emailTextfield.text , let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
                 if let e = error {
                     print(e.localizedDescription)
-                }else{
-                    
-                    self.performSegue(withIdentifier: "registerToMap", sender: self)
+                } else {
+                    self?.performSegue(withIdentifier: "registerToMap", sender: self)
                 }                
             }
         }
